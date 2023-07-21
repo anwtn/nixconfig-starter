d9abe06365d7479ff0439a11fa54d2778938f39f
Aaron Newton <aaron.newton@playtravel.com.au>
2023-07-21 18:11:07 +1000

    Added bootloader back in.

--- systems/nixos/configuration.nix
+++ systems/nixos/configuration.nix
@@ -13,6 +13,11 @@
     ../common/i3.nix
   ];
 
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
   # Setup keyfile
   boot.initrd.secrets = {
     "/crypto_keyfile.bin" = null;
