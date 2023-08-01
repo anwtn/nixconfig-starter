b3dc2a365c9702feeaaffb977f22caeb296b855b
Aaron Newton <aaron.newton@playtravel.com.au>
2023-08-01 18:39:30 +1000

    Use correct home-manager version.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.11";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     repo-nnn = {
