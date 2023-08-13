aa3a1a1572fef5ccdcf57d3134ab3cc51de2114d
Aaron Newton <aaron.newton@playtravel.com.au>
2023-08-13 13:05:43 +1000

    The problem may be that we weren't installing the NVIDIA drivers.

--- systems/nixos/configuration.nix
+++ systems/nixos/configuration.nix
@@ -81,7 +81,34 @@ in {
 
   # AJN 20230731 - see https://nixos.wiki/wiki/Nvidia#Nvidia_PRIME
   # Use NVIDIA Prime drivers.
-  services.xserver.videoDrivers = [ "nvidia" ];
+
+  # Make sure opengl is enabled
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Tell Xorg to use the nvidia driver (also valid for Wayland)
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is needed for most Wayland compositors
+    modesetting.enable = true;
+
+    # Use the open source version of the kernel module
+    # Only available on driver 515.43.04+
+    open = false;
+
+    # Enable the nvidia settings menu
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+  
+  # services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.prime = {
     # sync.enable = true;
     offload = {
@@ -93,6 +120,7 @@ in {
     intelBusId = "PCI:0:2:0";
     nvidiaBusId = "PCI:1:0:0";
   };
+  # END: NVIDIA configuration.
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
