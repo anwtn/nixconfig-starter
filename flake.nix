c199b19825372efe050762c8f447a6a2131e1340
Aaron Newton <aaron.newton@gmail.com>
2025-09-04 15:24:30 +1000

    Doing my usual weekly update.

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
               home-manager = {
                 useGlobalPkgs = true;
                 useUserPackages = true;
-                backupFileExtension = "backup_20250826t1540";
+                backupFileExtension = "backup_20250904t1408";
                 users.aaronnewton = {
                   imports = [
                     ./home/default.nix
